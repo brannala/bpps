9eb22db2355ae9fcb4eb6b0e49092a855f15b931
Bruce Rannala <brannala@ucdavis.edu>
2019-03-26 15:23:43 -0700

    Updates

---
+++ precache-manifest.d8239bed07021f03707c56623b410147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8bb83f5f8d3ec22276",
+    "url": "/bpps/static/css/main.eaa899aa.chunk.css"
+  },
+  {
+    "revision": "cf8bb83f5f8d3ec22276",
+    "url": "/bpps/static/js/main.47ce0aa4.chunk.js"
+  },
+  {
+    "revision": "35cb8ca4bb627f8be597",
+    "url": "/bpps/static/js/runtime~main.5990acac.js"
+  },
+  {
+    "revision": "8ff5b1b0b3e721f94e62",
+    "url": "/bpps/static/js/2.d6cb8338.chunk.js"
+  },
+  {
+    "revision": "ea575ee7d309abb938a04c9511bd2454",
+    "url": "/bpps/index.html"
+  }
+];
